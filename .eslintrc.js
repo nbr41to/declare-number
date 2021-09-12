0ffceef9046a24d84a103c99341200ca4bd70f77
nbr41to <nbr.41to@gmail.com>
2021-09-12 21:57:04 +0900

    .

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: ['next', 'eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:@next/next/recommended', 'prettier'],
+  rules: {
+    '@next/next/no-page-custom-font': 'off',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-empty-function': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    "import/newline-after-import": "warn",
+    "import/no-default-export": "warn",
+    "import/no-extraneous-dependencies": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": "warn",
+    quotes: ['error', 'single'],
+  },
+};
