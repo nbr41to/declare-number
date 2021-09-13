c066d2838997b8fe1e7232110e1328f7625c2f5c
nbr41to <nbr.41to@gmail.com>
2021-09-13 13:22:43 +0900

    .

--- pages/[roomId].tsx
+++ pages/[roomId].tsx
@@ -4,10 +4,6 @@ import { Room } from 'src/components/Room';
 const RoomPage: NextPage = () => {
   return (
     <div>
-      <h1>
-        Authors
-        <span>Lie</span>
-      </h1>
       <Room />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,8 @@ import { Top } from 'src/components/Top';
 const TopPage: NextPage = () => {
   return (
     <div>
-      <h1>
-        Authors
-        <span>Lie</span>
-      </h1>
+      <h1>Declare number</h1>
+      <h2>数を宣言する</h2>
       <Top />
     </div>
   );
